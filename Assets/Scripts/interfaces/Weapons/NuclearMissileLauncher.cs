9e713c1aeb222ecbabdb1f6016031b4b4fae7625
SjoerdJakobs <18284@ma-web.nl>
2016-05-31 00:23:00 +0200

    sync
    sync

---
+++ Assets/Scripts/interfaces/Weapons/NuclearMissileLauncher.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NuclearMissileLauncher : MonoBehaviour, IWeapon {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Shoot() {
+		Debug.Log("Alert, alert, we are launching a nuclear missile in 5, 4, 3, 2, 1....");
+	}
+}
