4564baf2e62b791be774a14a9f9704148372a57c
SjoerdJakobs <18284@ma-web.nl>
2016-06-17 08:31:18 +0200

    long terrain
    long terrain

---
+++ Assets/Scripts/activeTerrain.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class activeTerrain : MonoBehaviour {
+
+    private GameObject[] terrains;
+    private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        terrains = GameObject.FindGameObjectsWithTag("terrain");
+        StartCoroutine(checkIfInRange());	
+	}
+
+    IEnumerator checkIfInRange()
+    {
+        while(true)
+        {
+            foreach(GameObject terrainPart in terrains)
+            {
+                //print(terrains.Length);
+                if(terrainPart.transform.isDistanceSmallerThan(player.transform.position, 120f))
+                {
+                    terrainPart.SetActive(true);
+                }
+                else
+                {
+                    terrainPart.SetActive(false);
+                }
+            }
+            yield return new WaitForSeconds(0.25f);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
