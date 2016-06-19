ed78401c759b0f26a8d8a2400aa2aeab462ecb3a
SjoerdJakobs <18284@ma-web.nl>
2016-06-20 00:19:11 +0200

    whoops
    had met mn slaapdronken hoofd(gwn erg moe). niet gecommit maar we
    gesynced. :/ ook nog een script er uit gehaald dat niet word gebruikt.
    dat kan alleen maar verwarrinng geven.

--- Assets/testEnemy.cs
+++ Assets/testEnemy.cs
@@ -4,14 +4,42 @@ using System.Collections;
 public class testEnemy : LivingEntity
 {
 
+    private float playerLVL;
+    private float upgradeRate;
+    private GameObject target;
+
+
     protected override void Start()
     {
         base.Start();
+        setStats();
+        target = GameObject.FindGameObjectWithTag("Player");
+        playerLVL = target.GetComponent<LivingEntity>()._publicLVL;
 
     }
-	
-	// Update is called once per frame
-	void Update () {
+    void setStats()
+    {
+        Level = playerLVL;
+        upgradeRate = playerLVL * 1.75f;
+        xpOnDeath = xpOnDeath * upgradeRate;
+        maxHealth = maxHealth * upgradeRate;
+        healthRegen = healthRegen * upgradeRate; ;
+        maxMana = maxMana * upgradeRate;
+        manaRegen = manaRegen * upgradeRate;
+        armor = armor * upgradeRate;
+        armorPen = armorPen * upgradeRate;
+        magicResist = magicResist * upgradeRate;
+        magicPen = magicPen * upgradeRate;
+        moveMentspeed = moveMentspeed * upgradeRate;
+        tenacity = tenacity * upgradeRate;
+        sizeMod += sizeMod * 0.05f;
+        attackDamage = attackDamage * upgradeRate;
+        magicDamage = magicDamage * upgradeRate;
+        cooldownReduction = cooldownReduction * upgradeRate;
+    }
+
+    // Update is called once per frame
+    void Update () {
 	
 	}
 }
